732c24066e75d1073075bad5ed310f94133101fd
agricolamz <agricolamz@gmail.com>
2017-01-19 10:45:18 +0300

    exceptions addedd

--- sync.R
+++ sync.R
@@ -1,30 +1,42 @@
-#' Sync RStudio and GitHub via auto refreshe pages
+#' Sync RStudio and GitHub via auto refresh pages
 #'
-#' Takes a path vector, renders the site from Rmd files, changes .html files in the /docs subbderictory to auto refreshe pages and push it to GitHub via ssh key.
-#' @param x character vector of the languoids (can be written in lower case)
+#' Takes a path vector, renders the site from Rmd files, changes .html files in the /docs subbderictory to auto refresh pages and push it to GitHub via ssh key.
+#' @param path is a path to the derictory with Rmd files
+#' @param frequency is a integer that set auto refresh frequency
+#' @param exceptions is a vector of names of Rmd files that should stay without auto refresh
 #' @author George Moroz <agricolamz@gmail.com>
 #' @seealso \code{\link{aff.lang}}, \code{\link{country.lang}}, \code{\link{iso.lang}}, \code{\link{lat.lang}}, \code{\link{long.lang}}
 #' @examples
-#' area.lang("Adyghe")
-#' area.lang(c("Adyghe", "Aduge"))
+#' sync()
+#' sync(exceptions = "Lec_1_Course_description.html")
+#' sync(exceptions = list.files(pattern="Lec.*\\.html"))
 #' @export
 
 
-sync <- function(path, frequency = 5){
+sync <- function(path = getwd(), frequency = 5, exceptions = NULL){
   # render site
   setwd(path)
   rmarkdown::render_site()
   
+  # if there are exceptions
+  if (!is.null(exceptions)){
+    files <- setdiff(list.files(pattern=".*\\.html"), exceptions)
+  } else {files <- list.files(pattern=".*\\.html")}
+
   # add a 10 seconds auto refresh
   setwd(paste0(path, "/docs"))
-  sapply(list.files(pattern="*.html"), function(x){
+  sapply(files, function(x){
     html <- readLines(x)
     html[8] <- paste0('<meta http-equiv="refresh" content="', frequency, '"/>')
     writeLines(html, x)
     }
   )
-  # commit on github
+  
+  # push to github
   system(command = paste0('cd ',
          path,
          '; git add --all; mydate=`date`; git commit -m "$mydate"; git push -u origin master'))
+  
+  # change the working directory back
+  setwd(path)
   }
